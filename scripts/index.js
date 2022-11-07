9b8e6dd5351d40846e306094617a212a9e390d2a
Luís Nunes <luis.ebn98@gmail.com>
2022-11-07 16:18:23 -0300

    fix: remover condicional sem uso

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 import { receberForm } from "./inputs.js"
+import { login } from "./request.js"
 
 const formularioLogin = document.querySelector(".div-inputs")
 
 formularioLogin.addEventListener("submit", (event) =>{
     event.preventDefault()
     let corpo = receberForm(formularioLogin.elements)
+    login(corpo)
 })
