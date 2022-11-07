e949dfbf6b95dc25d6f774bd5ba7f854064c9e95
Luís Nunes <luis.ebn98@gmail.com>
2022-11-07 18:08:39 -0300

    hotfix: deletar console

--- scripts/request.js
+++ scripts/request.js
@@ -13,7 +13,6 @@ export async function cadastroUsuario(usuario) {
 
         let novoUsuarioJson = await novoUsuario.json()
 
-    console.log(novoUsuarioJson)        
         return novoUsuarioJson
 
     } catch (err) {
