869007824af0d340d0b3fcc32e8e7e8f8510e11e
Luís Nunes <luis.ebn98@gmail.com>
2022-11-08 16:45:42 -0300

    add/imagem perfil

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -2,6 +2,9 @@ import { meuPerfil, meusPets } from "../../scripts/request.js"
 
 const token = JSON.parse(localStorage.getItem("token"))
 const dadosPessoais = document.querySelector(".dados")
+const imagemPerfil = document.querySelector(".cabecalho img")
+
+console.log(imagemPerfil)
 
 let perfil = await meuPerfil(token)
 let listaMeusPets = await meusPets(token)
@@ -11,3 +14,5 @@ dadosPessoais.insertAdjacentHTML("afterbegin", `
                 <h2><span>E-mail:</span> ${perfil.email}</h2>
                 <h2><span>Você adotou:</span> ${listaMeusPets.length} pets</h2>
 `)
+
+imagemPerfil.src = perfil.avatar_url
