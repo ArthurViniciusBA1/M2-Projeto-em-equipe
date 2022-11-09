eb096d353798391f6b3594a8c8a8e5663666e553
Luís Nunes <luis.ebn98@gmail.com>
2022-11-09 09:02:14 -0300

    fix/segue convencao de classes

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,7 +5,7 @@ const botaoDrop = document.querySelector(".buttonDropdown");
 const navegacao = document.querySelector("nav");
 
 botaoDrop.addEventListener("click", () => {
-  navegacao.classList.toggle("noShow");
+  navegacao.classList.toggle("naoAparecer");
 });
 
 async function renderizaCardsPets() {
